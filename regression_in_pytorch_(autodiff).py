990eed1b4ab57b24f3fe99506a9a4245e8fcadb6
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-10-05 00:35:29 +0600

    Update regression_in_pytorch_(autodiff).py

--- regression_in_pytorch_(autodiff).py
+++ regression_in_pytorch_(autodiff).py
@@ -28,14 +28,14 @@ b=th.tensor([0.1]).requires_grad_()
 print(b)
 
 
-def regression_plot(my_x,my_y,my_m,my_b):
-    
+def regression_plot(my_x,my_y,my_m,my_b):    
     fig,ax=plt.subplots()
     ax.scatter(my_x,my_y)
     x_min,x_max=ax.get_xlim()
     y_min,y_max=my_m*x_min+my_b,my_m*x_max+my_b
     ax.set_xlim([x_min,x_max])
     _=ax.plot([x_min,x_max],[y_min,y_max])
+
 
 
 regression_plot(x, y, m, b)    
